206a75fcf967545bc5c45383e32b464f24cdf244
Troy Williams <troyw@waikato.ac.nz>
2013-12-17 15:54:06 +1300

    Up version to 2.6

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->component = 'repository_soundcloud';   // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2013051400;            // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013051400;            // Requires this Moodle version
-$plugin->release   = '2.5';                 // Human-friendly version name
+$plugin->version   = 2013111800;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013111800;            // Requires this Moodle version
+$plugin->release   = '2.6';                 // Human-friendly version name http://docs.moodle.org/dev/Releases
 $plugin->maturity  = MATURITY_BETA;         // This version's maturity level
